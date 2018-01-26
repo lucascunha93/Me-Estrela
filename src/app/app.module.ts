7cc14545b2f81efbff5fd7bd00e143f9ecd95423
Lucas Cunha <oliveira.lucasc93@hotmail.com>
2018-01-25 23:18:25 -0200

    Implementando o Angular 4

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ModalComponent } from './login/modal/modal.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    ModalComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
